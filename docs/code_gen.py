26ac24a5a65bb77c771f99743113faae4967c131
Myoungwoo Jang <codeart@gmail.com>
2022-06-23 15:57:32 +0900

    API Code generation  - 국내주식시세 모든 리스트 사용 가능  - stdout에 있는 내요을 copy/paste

--- docs/code_gen.py
+++ docs/code_gen.py
@@ -7,11 +7,12 @@ api_doc_file = "KIS Developers API명세서_220616.xlsx"
 wb = openpyxl.load_workbook(api_doc_file)
 print(wb.sheetnames)
 
-sheet_names = ['OAuth인증', '국내주식주문', '국내주식시세', '국내주식실시간', '국내선물옵션주문', '국내선물옵션시세', '해외주식주문', '해외주식현재가']
+sheet_names = ['OAuth인증', '국내주식주문', '국내주식시세', '국내주식실시간',
+               '국내선물옵션주문', '국내선물옵션시세', '해외주식주문', '해외주식현재가']
 sheet = wb[sheet_names[2]]
 
 
-def extract_api_row_index():
+def extract_api_list_from_sheet():
     api_row_nums = []
     count = 0
     for row in sheet.iter_rows(values_only=False):
@@ -29,7 +30,7 @@ def extract_api_row_index():
 def extract_api_parameters(name):
     _row_index = 0
     _header = {}
-    _found = {"API명" : False, "Request": False}
+    _found = {"API명": False, "Request": False}
     for row in sheet.iter_rows(values_only=False):
 
         # print(row[1].value, name)
@@ -51,8 +52,8 @@ def extract_api_parameters(name):
             continue
 
         if _found["Request"] and row[0].value == "tr_id" and row[5].value == "[실전/모의투자]":
-            # print(f"{row[0].value}")
-            _header[f"{row[0].value}"] = f"{sheet[row[0].row + 1][5].value}".split(":")[0].strip()
+            _header[f"{row[0].value}"] = \
+                f"{sheet[row[0].row + 1][5].value}".split(":")[0].strip()
 
             continue
 
@@ -65,16 +66,9 @@ def extract_api_parameters(name):
     return _row_index, _header
 
 
-def api_row_position(row_index):
-
-    # summary
-
+def get_api_basic_info(row_index):
     # api_name = f"{sheet[row_index][0].value}"
-    api_info = f"{sheet[row_index+1][1].value}"
-    # api_note = f"{sheet[row_index+1][0].value}"
-    # print(api_name)
-    # print(api_info)
-    # print(api_note)
+    api_info = f"{sheet[row_index + 1][1].value}"
     api_params_raw = api_info.split("\n")
     api_params = {}
     for data in api_params_raw:
@@ -87,17 +81,34 @@ def generate_code(api_summary, header):
     print("pub fn API_NAME_XXX(&self) -> KisResult<serde_json::Value> {")
     print(f"    let url = \"{api_summary['URL']}\";")
     print(f'    let headers = [("tr_id", "{header["tr_id"]}")];')
-    print(f'    let query = [("fid_cond_mrkt_div_code", "J"), ("fid_input_iscd", ticker)];')
-    print(f'    let req = self.make_request(url, RequestType::{api_summary["Method"]}, &headers, &query)?;')
+    print(
+        f'    let query = [("fid_cond_mrkt_div_code", "J"), ("fid_input_iscd", ticker)];')
+    print(
+        f'    let req = self.make_request(url, RequestType::{api_summary["Method"]}, &headers, &query)?;')
     print('')
     print(f'    self.send_request(req)')
     print("}")
 
 
 if __name__ == '__main__':
-    apis_name_list = ["주식현재가 시세[v1_국내주식-008]", "주식현재가 체결[v1_국내주식-009]"]
-    row_index, header = extract_api_parameters(apis_name_list[1])
-    # print(row_index, header)
-    summary = api_row_position(row_index)
-    generate_code(summary, header)
-
+    # All api info in a sheet
+    # api_list = extract_api_list_from_sheet()
+    # for item in api_list:
+    #     # print('"' + item[0] + '"' + ",")
+    #     print("- [ ] "+item[0])
+    # exit()
+    apis_name_list = [
+        "주식현재가 시세[v1_국내주식-008]",
+        "주식현재가 체결[v1_국내주식-009]",
+        "주식현재가 일자별[v1_국내주식-010]",
+        "주식현재가 호가 예상체결[v1_국내주식-011]",
+        "주식현재가 투자자[v1_국내주식-012]",
+        "주식현재가 회원사[v1_국내주식-013]",
+        "ELW현재가 시세[v1_국내주식-014]",
+        "국내주식기간별시세(일/주/월/년)[v1_국내주식-016]",
+        "국내주식업종기간별시세(일/주/월/년)[v1_국내주식-021]",
+    ]
+    for api_name in apis_name_list:
+        begin_row_num, header = extract_api_parameters(api_name)
+        summary = get_api_basic_info(begin_row_num)
+        generate_code(summary, header)
